ddf8973dea2b125b7185b5501b7ac870a66ee6a2
Diego Maidana <die.maidana1@gmail.com>
2026-05-19 22:53:27 -0300

    feat: Product Entity created

---
+++ src/main/java/com/loki/tesis/products/Product.java
@@ -0,0 +1,46 @@
+package com.loki.tesis.products;
+
+import com.github.f4b6a3.uuid.UuidCreator;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "products")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Product {
+    @Id
+    @Column(updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    private BigDecimal priceMin;
+
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void onCreate() {
+        if(this.id == null) {
+            id = UuidCreator.getTimeOrderedEpoch();
+        }
+
+        createdAt = LocalDateTime.now();
+    }
+}
