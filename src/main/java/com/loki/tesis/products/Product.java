f41a20616888c60a155a61be7e4092372b17c4bd
Diego Maidana <die.maidana1@gmail.com>
2026-06-22 22:44:18 -0300

    fix: added @Enumerated in Product and added @Valid and @RequestBody in update method in CategoryController

--- src/main/java/com/loki/tesis/products/Product.java
+++ src/main/java/com/loki/tesis/products/Product.java
@@ -43,6 +43,7 @@ public class Product {
     private LocalDateTime updatedAt;
 
     @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
     private ProductStatus status;
 
     @Column(name = "stock", nullable = false)
