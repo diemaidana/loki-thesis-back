0b5a479f7f62c3e47c2dc16df858a4bc335430a8
Diego Maidana <die.maidana1@gmail.com>
2026-06-26 15:20:23 -0300

    fix: added @EnableSpringDataWebSupport to serialize Page using stable DTO with JSON structure.

--- src/main/java/com/loki/tesis/Application.java
+++ src/main/java/com/loki/tesis/Application.java
@@ -2,9 +2,11 @@ package com.loki.tesis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 @EnableAsync
 public class Application {
 
