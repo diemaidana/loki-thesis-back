a0e81341064dfe066ad9d5002e557d0924e4eff9
Diego Maidana <die.maidana1@gmail.com>
2026-08-19 21:32:57 -0300

    fix: changed CartResponseDTO attribute to match the entity: cartItems

--- src/main/java/com/loki/tesis/cart/dto/CartResponseDTO.java
+++ src/main/java/com/loki/tesis/cart/dto/CartResponseDTO.java
@@ -8,6 +8,6 @@ public record CartResponseDTO(
         UUID cartCode,
         BigDecimal total,
         Integer itemCount,
-        List<CartItemResponseDTO> items
+        List<CartItemResponseDTO> cartItems
 ) {
 }
